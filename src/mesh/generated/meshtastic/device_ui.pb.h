be59368949c68a9d27ece6bf1812063413b6de37
Michael A. Cojocari <michael.cojocari@gmail.com>
2026-03-18 17:20:10 -0400

    Stuff

--- src/mesh/generated/meshtastic/device_ui.pb.h
+++ src/mesh/generated/meshtastic/device_ui.pb.h
@@ -25,7 +25,11 @@ typedef enum _meshtastic_Theme {
     /* Light */
     meshtastic_Theme_LIGHT = 1,
     /* Red */
-    meshtastic_Theme_RED = 2
+    meshtastic_Theme_RED = 2,
+    /* Grey */
+    meshtastic_Theme_GREY = 3,
+    /* Dark Blue - comfortable for night reading */
+    meshtastic_Theme_DARK_BLUE = 4
 } meshtastic_Theme;
 
 /* Localization */
@@ -206,8 +210,8 @@ extern "C" {
 #define _meshtastic_CompassMode_ARRAYSIZE ((meshtastic_CompassMode)(meshtastic_CompassMode_FREEZE_HEADING+1))
 
 #define _meshtastic_Theme_MIN meshtastic_Theme_DARK
-#define _meshtastic_Theme_MAX meshtastic_Theme_RED
-#define _meshtastic_Theme_ARRAYSIZE ((meshtastic_Theme)(meshtastic_Theme_RED+1))
+#define _meshtastic_Theme_MAX meshtastic_Theme_DARK_BLUE
+#define _meshtastic_Theme_ARRAYSIZE ((meshtastic_Theme)(meshtastic_Theme_DARK_BLUE+1))
 
 #define _meshtastic_Language_MIN meshtastic_Language_ENGLISH
 #define _meshtastic_Language_MAX meshtastic_Language_TRADITIONAL_CHINESE
